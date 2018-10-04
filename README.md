b57b626ff8fb8495ab08599f4402817102d6238f
liu364515154 <liuyafei.cf@163.com>
2018-10-04 14:24:44 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,4 @@
+# 简单输入法案例
+
+## 按键CODE
+参考android.view.KeyEvent属性
---
+++ app/src/main/java/com/loocup/inputdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.loocup.inputdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
